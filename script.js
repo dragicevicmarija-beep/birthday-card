906b7212387ae86b124279631ec917eb9c1117f3
Marija D. <marijad1234567@gmail.com>
2026-07-20 20:30:11 +0200

    loadingscreen added

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const circle = document.getElementById("circle");
 const popSound = document.getElementById("popSound");
 const bgMusic = document.getElementById("bgMusic");
 const kabum = document.getElementById("kabum");
-//phases
+//screens
+const loadingScreen = document.getElementById("loadingScreen");
 const questionScreen = document.getElementById("questionScreen");
 const giftScreen = document.getElementById("giftScreen");
 const noScreen = document.getElementById("noScreen");
@@ -18,40 +19,20 @@ const noButton = document.getElementById("noButton");
 const giftButton = document.getElementById("giftButton");
 
 let rotationAngle = 0;
-let audioReady = false;
 
-//set all the divs to none
+const pedjaButton = document.getElementById("pedja");
+pedjaButton.addEventListener("click", () => {
+  loadingScreen.style.display = "none";
+  questionScreen.style.display = "flex";
+});
+
 giftScreen.style.display = "none";
+
 noButton.addEventListener("click", () => {
   questionScreen.style.display = "none";
   noScreen.style.display = "block";
 });
 yesButton.addEventListener("click", () => {
-  //load audios
-  /*kabum
-    .play()
-    .then(() => {
-      kabum.pause();
-      //kabum.currentTime = 0;
-      audioReady = true;
-    })
-    .catch(() => {});
-  popSound
-    .play()
-    .then(() => {
-      popSound.pause();
-      //popSound.currentTime = 0;
-      audioReady = true;
-    })
-    .catch(() => {});
-  bgMusic
-    .play()
-    .then(() => {
-      bgMusic.pause();
-      //bgMusic.currentTime = 0;
-    })
-    .catch(() => {});*/
-  //GoToGiftScreen;
   questionScreen.style.display = "none";
   yesButton.style.display = "none";
   noButton.style.display = "none";
@@ -59,43 +40,31 @@ yesButton.addEventListener("click", () => {
   giftButton.addEventListener("click", () => {
     goToMina();
   });
-  //goToGiftScreen();
 });
 
-//function goToGiftScreen() {}
-//function GoToGiftScreen() {}
 function goToMina() {
   giftScreen.style.display = "none";
   mina.style.display = "block";
   mina.classList.add("mina-spin");
-
-  // Play pop sound once while mina rotates
-  //if (audioReady) {
   popSound.currentTime = 0;
   popSound.play().catch(() => {});
-  //}
 
   setTimeout(() => {
-    // mina stays visible briefly, then explosion
+    // mina stays visible fro a sec, then explosion
     setTimeout(() => {
       mina.style.display = "none";
       explosion.style.display = "block";
 
-      //if (audioReady) {
       kabum.currentTime = 0;
       kabum.play().catch(() => {});
-      //}
 
       setTimeout(() => {
         kabum.pause();
         explosion.style.display = "none";
         finalScreen.style.display = "block";
 
-        // Start background music
-        //if (audioReady) {
         bgMusic.currentTime = 0;
         bgMusic.play().catch(() => {});
-        //}
 
         document.addEventListener("keydown", handleSpacebar);
       }, 1000); // explosion duration
